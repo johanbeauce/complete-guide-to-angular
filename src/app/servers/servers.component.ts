9ae5c59862223625a178a5745c82dad1805f7e77
Johan BEAUCE <jbeauce@pictet-technologies.com>
2020-06-02 10:00:22 +0200

    Add property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
+  serverEnable = true;
+
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.serverEnable = false;
+    }, 2000);
   }
 
 }
